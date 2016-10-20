d2f267c3d73d08217a4077a9e1d2154bddc82fb6
Lawrence Dawson <lawrencedawson@hotmail.co.uk>
2016-10-20 20:17:10 +0100

    added a test for winning

--- jasmine/spec/tickTackToeSpec.js
+++ jasmine/spec/tickTackToeSpec.js
@@ -1,22 +1,29 @@
 describe("Game", function() {
 
-beforeEach(function() {
-  game = new Game();
-  winningSolutions = [[1,2,3,],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]];
-  grid = [[1,2,3],
-              [4,5,6],
-              [7,8,9]];
-  });
+  beforeEach(function() {
+    game = new Game();
+    });
 
-  it("should should initialize with an array of winning solutions", function() {
-    expect(winningSolutions).toEqual([[1,2,3,],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]);
-  });
+  describe('initialize', function() {
 
-  it("should should initialize with a TTT grid array", function() {
-    expect(grid).toEqual([[1,2,3],
-                                [4,5,6],
-                                [7,8,9]]);
-  });
+    it("should should initialize with an array of winning solutions", function() {
+      expect(winningSolutions).toEqual([[1,2,3,],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]);
+    });
+
+    it("should should initialize with a TTT grid array", function() {
+      expect(grid).toEqual([[1,2,3],
+                            [4,5,6],
+                            [7,8,9]]);
+      });
+    });
+
+describe('player makes a choice', function() {
+  beforeEach(function() {
+    winningSolutions = [[1,2,3,],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]];
+    grid = [[1,2,3],
+            [4,5,6],
+            [7,8,9]];
+    });
 
   it("a player can make an X on the grid", function() {
     game.choose('X',1,grid);
@@ -43,6 +50,19 @@ beforeEach(function() {
     game.choose('X',1, grid);
     expect(winningSolutions).toEqual([['X',2,3,],[4,5,6],[7,8,9],['X',4,7],[2,5,8],[3,6,9],['X',5,9],[3,5,7]]);
   });
+});
 
-
+describe('winning', function() {
+  beforeEach(function() {
+    winningSolutions = [[1,'O','O',],[4,'X','O'],[7,8,'X'],[1,4,7],['O','X',8],['O',6,'X'],[1,'X','X'],['O','X',7]];
+    grid = [[1,'O','O'],
+            [4,'X','O'],
+            [7,8,'X']];
+    });
+    it('a player wins the game', function() {
+      game.choose('X', 1, grid);
+      console.log(winningSolutions);
+      expect(game.winner).toEqual('X');
+    });
+  });
 });
