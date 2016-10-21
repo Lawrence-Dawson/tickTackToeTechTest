447e10bd5de020dbd4c81b7e15f32e449dee3154
Lawrence Dawson <lawrencedawson@hotmail.co.uk>
2016-10-21 13:19:27 +0100

    removed global variables

--- jasmine/spec/tickTackToeSpec.js
+++ jasmine/spec/tickTackToeSpec.js
@@ -7,11 +7,11 @@ describe("Game", function() {
   describe('initialize', function() {
 
     it("should should initialize with an array of winning solutions", function() {
-      expect(winningSolutions).toEqual([[1,2,3,],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]);
+      expect(game._winningSolutions).toEqual([[1,2,3,],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]);
     });
 
     it("should should initialize with a TTT grid array", function() {
-      expect(grid).toEqual([[1,2,3],
+      expect(game._grid).toEqual([[1,2,3],
                             [4,5,6],
                             [7,8,9]]);
       });
@@ -19,58 +19,59 @@ describe("Game", function() {
 
 describe('player makes a choice', function() {
   beforeEach(function() {
-    winningSolutions = [[1,2,3,],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]];
-    grid = [[1,2,3],
+    game._winningSolutions = [[1,2,3,],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]];
+    game._grid = [[1,2,3],
             [4,5,6],
             [7,8,9]];
     });
 
   it("a player can make an X on the grid", function() {
-    game.choose('X',1,grid);
-    expect(grid).toEqual([['X',2,3],[4,5,6],[7,8,9]]);
+    game.play('X',1);
+    expect(game._grid).toEqual([['X',2,3],[4,5,6],[7,8,9]]);
   });
 
+
   it("a player can make an O on the grid", function() {
-    game.choose('O',1,grid);
-    expect(grid).toEqual([['O',2,3],[4,5,6],[7,8,9]]);
+    game.play('O',1);
+    expect(game._grid).toEqual([['O',2,3],[4,5,6],[7,8,9]]);
   });
 
   it("a player cannot choose a location already taken", function() {
-    game.choose('X',1,grid);
-    game.choose('O',1,grid);
-    expect(grid).toEqual([['X',2,3],[4,5,6],[7,8,9]]);
+    game.play('X',1);
+    game.play('O',1);
+    expect(game._grid).toEqual([['X',2,3],[4,5,6],[7,8,9]]);
   });
 
   it("the winningSolutions array can be altered", function() {
-    game.choose('X',1, winningSolutions);
-    expect(winningSolutions).toEqual([['X',2,3,],[4,5,6],[7,8,9],['X',4,7],[2,5,8],[3,6,9],['X',5,9],[3,5,7]]);
+    game.play('X',1);
+    expect(game._winningSolutions).toEqual([['X',2,3,],[4,5,6],[7,8,9],['X',4,7],[2,5,8],[3,6,9],['X',5,9],[3,5,7]]);
   });
 
   it("the winningSolutions array can be altered", function() {
-    game.choose('X',1, grid);
-    expect(winningSolutions).toEqual([['X',2,3,],[4,5,6],[7,8,9],['X',4,7],[2,5,8],[3,6,9],['X',5,9],[3,5,7]]);
+    game.play('X',1);
+    expect(game._winningSolutions).toEqual([['X',2,3,],[4,5,6],[7,8,9],['X',4,7],[2,5,8],[3,6,9],['X',5,9],[3,5,7]]);
   });
 });
 
 describe('winning', function() {
   beforeEach(function() {
-    winningSolutions = [[1,'O','O',],[4,'X','O'],[7,8,'X'],[1,4,7],['O','X',8],['O',6,'X'],[1,'X','X'],['O','X',7]];
-    grid = [[1,'O','O'],
+    game._winningSolutions = [[1,'O','O',],[4,'X','O'],[7,8,'X'],[1,4,7],['O','X',8],['O',6,'X'],[1,'X','X'],['O','X',7]];
+    game._grid = [[1,'O','O'],
             [4,'X','O'],
             [7,8,'X']];
     });
     it('game knows when X is a winner', function() {
-      game.choose('X', 1, grid);
+      game.play('X', 1);
       expect(game.winner()).toEqual("X");
     });
 
     it('game knows when O is a winner', function() {
-      game.choose('O', 1, grid);
+      game.play('O', 1);
       expect(game.winner()).toEqual("O");
     });
 
     it('game knows when there is not a winner', function() {
-      game.choose('X', 7, grid);
+      game.play('X', 7);
       expect(game.winner('X')).not.toBe('X');
     });
 
--- jasmine/src/tickTackToe.js
+++ jasmine/src/tickTackToe.js
@@ -2,35 +2,38 @@
 
 
 function Game() {
+  this._winningSolutions = [[1,2,3,],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]];
+  this._grid = [[1,2,3],
+              [4,5,6],
+              [7,8,9]];
 
 }
 
-var winningSolutions = [[1,2,3,],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]];
-var grid = [[1,2,3],
-            [4,5,6],
-            [7,8,9]];
+  Game.prototype.play = function (letter, gridNum) {
 
-  Game.prototype.choose = function (letter, gridNum, array) {
-    if (array.indexOf(letter) === false) {
-      throw new Error("That location choice has already been taken!");
-      }
-    else {
-      for(var i = 0; i < array.length; i++) {
-          var arrSection = array[i];
+  choose(letter, gridNum, this._winningSolutions);
+  choose(letter, gridNum, this._grid);
 
-      for(var j = 0; j < arrSection.length; j++) {
-          if (arrSection[j] === gridNum) {
-                arrSection[j] = letter;
-                game.choose(letter, gridNum, winningSolutions);
+  function choose(letter, gridNum, array) {
+      if (array.indexOf(letter) === false) {
+        throw new Error("That location choice has already been taken!");
+        }
+      else {
+        for(var i = 0; i < array.length; i++) {
+            var arrSection = array[i];
+
+        for(var j = 0; j < arrSection.length; j++) {
+            if (arrSection[j] === gridNum) {
+                  arrSection[j] = letter;
+                }
               }
             }
           }
         }
-      };
 
   Game.prototype.winner = function () {
-    for (var i = 0; i < winningSolutions.length; i++) {
-      var arrSection = winningSolutions[i].toString();
+    for (var i = 0; i < this._winningSolutions.length; i++) {
+      var arrSection = this._winningSolutions[i].toString();
     if (arrSection === "X,X,X") {
       return arrSection[0];
     }
@@ -40,4 +43,5 @@ var grid = [[1,2,3],
       }
     }
   }
+};
 };
