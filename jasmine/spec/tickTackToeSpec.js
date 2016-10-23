c4d2bbe332058e37bab3874afedbb0a5ed201316
Lawrence Dawson <lawrencedawson@hotmail.co.uk>
2016-10-23 17:44:45 +0100

    fixed draw function, removed wrapper function from recursive function by creating a default parameter, game now returns a status after every turn, all major functionality is complete

--- jasmine/spec/tickTackToeSpec.js
+++ jasmine/spec/tickTackToeSpec.js
@@ -17,84 +17,78 @@ describe("Game", function() {
       });
     });
 
-describe('player makes a choice', function() {
-  beforeEach(function() {
-    game._winningSolutions = [[1,2,3,],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]];
-    game._grid = [[1,2,3],
-            [4,5,6],
-            [7,8,9]];
-    });
-
-  it("a player can make an X on the grid", function() {
-    game.play('X',1);
-    expect(game._grid).toEqual([['X',2,3],[4,5,6],[7,8,9]]);
-  });
-
-
-  it("a player can make an O on the grid", function() {
-    game.play('O',1);
-    expect(game._grid).toEqual([['O',2,3],[4,5,6],[7,8,9]]);
-  });
-
-  it("a player cannot choose a location already taken", function() {
-    game.play('X',1);
-    game.play('O',1);
-    expect(game._grid).toEqual([['X',2,3],[4,5,6],[7,8,9]]);
-  });
-
-  it("the winningSolutions array can be altered", function() {
-    game.play('X',1);
-    expect(game._winningSolutions).toEqual([['X',2,3,],[4,5,6],[7,8,9],['X',4,7],[2,5,8],[3,6,9],['X',5,9],[3,5,7]]);
-  });
-
-  it("the winningSolutions array can be altered", function() {
-    game.play('X',1);
-    expect(game._winningSolutions).toEqual([['X',2,3,],[4,5,6],[7,8,9],['X',4,7],[2,5,8],[3,6,9],['X',5,9],[3,5,7]]);
-  });
-
-  it("each play reduces number of plays left by one", function() {
-    game.play('X',1);
-    expect(game._plays).toEqual(8);
-  });
-
-
-});
-
-describe('winning', function() {
-  beforeEach(function() {
-    game._winningSolutions = [[1,'O','O',],[4,'X','O'],[7,8,'X'],[1,4,7],['O','X',8],['O',6,'X'],[1,'X','X'],['O','X',7]];
-    game._grid = [[1,'O','O'],
-            [4,'X','O'],
-            [7,8,'X']];
-    });
-    it('game knows when X is a winner', function() {
-      game.play('X', 1);
-      expect(game.winner()).toEqual("X wins!");
-    });
-
-    it('game knows when O is a winner', function() {
-      game.play('O', 1);
-      expect(game.winner()).toEqual("O wins!");
-    });
-
-    it('game knows when there is not a winner', function() {
-      game.play('X', 7);
-      expect(game.winner('X')).not.toBe('X');
-    });
-
-  });
-
-  describe('drawing', function() {
+  describe('player makes a choice', function() {
     beforeEach(function() {
-      game._winningSolutions = [[1,'X','O',],['O','O','X'],['X','O','X'],['X','O','X'],['X','O','O'],['O','X','X'],['X','O','X'],['O','O','X']];
+      game._winningSolutions = [[1,2,3,],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]];
+      game._grid = [[1,2,3],
+              [4,5,6],
+              [7,8,9]];
+    });
+
+    it("a player can make an X on the grid", function() {
+      game.choose('X',1);
+      expect(game._grid).toEqual([['X',2,3],[4,5,6],[7,8,9]]);
+    });
+
+
+    it("a player can make an O on the grid", function() {
+      game.choose('O',1);
+      expect(game._grid).toEqual([['O',2,3],[4,5,6],[7,8,9]]);
+    });
+
+    it("a player cannot choose a location already taken", function() {
+      game.choose('X',1);
+      game.choose('O',1);
+      expect(game._grid).toEqual([['X',2,3],[4,5,6],[7,8,9]]);
+    });
+
+    it("the winningSolutions array can be altered", function() {
+      game.choose('X',1);
+      expect(game._winningSolutions).toEqual([['X',2,3,],[4,5,6],[7,8,9],['X',4,7],[2,5,8],[3,6,9],['X',5,9],[3,5,7]]);
+    });
+
+    it("the winningSolutions array can be altered", function() {
+      game.choose('X',1);
+      expect(game._winningSolutions).toEqual([['X',2,3,],[4,5,6],[7,8,9],['X',4,7],[2,5,8],[3,6,9],['X',5,9],[3,5,7]]);
+    });
+  });
+
+  describe('winning', function() {
+    beforeEach(function() {
+      game._winningSolutions = [[1,'O','O',],[4,'X','O'],[7,8,'X'],[1,4,7],['O','X',8],['O',6,'X'],[1,'X','X'],['O','X',7]];
+      game._grid = [[1,'O','O'],
+              [4,'X','O'],
+              [7,8,'X']];
+      });
+      it('game knows when X is a winner', function() {
+        game.choose('X', 1);
+        expect(game.winner()).toEqual("X wins!");
+      });
+
+      it('game knows when O is a winner', function() {
+        game.choose('O', 1);
+        expect(game.winner()).toEqual("O wins!");
+      });
+
+      it('game knows when there is not a winner', function() {
+        game.choose('X', 7);
+        expect(game.winner()).not.toBe('X');
+      });
+    });
+
+    describe('drawing', function() {
+      beforeEach(function() {
+      game._winningSolutions = [[1,'X','O',],['O','O','X'],['X','O','X'],['X','O','X'],['X','O','O'],['O','X','X'],
+      ['X','O','X'],['O','O','X']];
       game._grid = [[1,'X','O'],
                     ['O','O','X'],
                     ['X','O','X']];
-      game._plays = 1;
       });
 
       it('game knows when theres a draw', function() {
-        expect(game.play('X', 1)).toEqual('Its a draw!');
+        game.choose('X', 1);
+        expect(game.gameStatus()).toEqual('Its a draw!');
       });
+
     });
-});
+  });
--- jasmine/src/tickTackToe.js
+++ jasmine/src/tickTackToe.js
@@ -3,53 +3,60 @@
 
 function Game() {
   this._winningSolutions = [[1,2,3,],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]];
-  this._grid = [[1,2,3],
-              [4,5,6],
-              [7,8,9]];
-  this._plays = 9;
+  this._grid =  [[1,2,3],
+                [4,5,6],
+                [7,8,9]];
+  }
 
-}
+  Game.prototype.choose = function (letter, gridNum, array) {
+    var array = (typeof array !== 'undefined') ? array : this._grid;
+      if (array.indexOf(letter) === false) {
+        throw new Error("That place is already taken!");
+        }
+      else {
+        for(var i = 0; i < array.length; i++) {
+            var arrayInArray = array[i];
 
-  Game.prototype.play = function (letter, gridNum) {
-    this._plays -= 1;
-    choose(letter, gridNum, this._grid);
-
-    function choose(letter, gridNum, array) {
-        if (array.indexOf(letter) === false) {
-          throw new Error("That place is already taken!");
-          }
-        else {
-          for(var i = 0; i < array.length; i++) {
-              var arrSection = array[i];
-
-          for(var j = 0; j < arrSection.length; j++) {
-              if (arrSection[j] === gridNum) {
-                  arrSection[j] = letter;
-                  choose(letter, gridNum, game._winningSolutions);
-              }
+      for(var j = 0; j < arrayInArray.length; j++) {
+          if (arrayInArray[j] === gridNum) {
+              arrayInArray[j] = letter;
+              game.choose(letter, gridNum, this._winningSolutions);
             }
           }
         }
       }
-
-      game.winner();
       return game.gameStatus();
-      };
+  };
 
-    Game.prototype.winner = function () {
-      for (var i = 0; i < this._winningSolutions.length; i++) {
-        var arrSection = this._winningSolutions[i].toString();
-        if (arrSection === "X,X,X") {
-          return arrSection[0] + " wins!";
-        }
-        else if (arrSection === "O,O,O") {
-            return arrSection[0] + " wins!";
-        }
+  Game.prototype.gameStatus = function () {
+    if (arraySum(this._grid) === 0) {
+      return 'Its a draw!';
+    }
+    else {
+      return game.winner();
+    }
+  };
+
+  Game.prototype.winner = function () {
+    for (var i = 0; i < this._winningSolutions.length; i++) {
+      var arrayInArray = this._winningSolutions[i].toString();
+      if (arrayInArray === "X,X,X") {
+        return arrayInArray[0] + " wins!";
       }
-    };
+      else if (arrayInArray === "O,O,O") {
+          return arrayInArray[0] + " wins!";
+      }
+    }
+  };
 
-    Game.prototype.gameStatus = function () {
-      if (this._plays === 0 ) {
-        return 'Its a draw!';
-        }
-    };
+  function arraySum(array) {
+    var sum=0;
+    for(var a=0;a<array.length;a++){
+       if(typeof array[a]=="number"){
+           sum+=array[a];
+       }else if(array[a] instanceof Array){
+           sum+=arraySum(array[a]);
+         }
+      }
+    return sum;
+  }
